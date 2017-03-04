c223f47e53be718438a76f69e7a223f006d11881
tvrcgo <tvrcgo@gmail.com>
2017-03-04 14:22:55 +0800

    Common server side template.

--- app/controller/home.js
+++ app/controller/home.js
@@ -1,8 +1,10 @@
 'use strict';
 
 exports.index = function* index() {
-  // this.body = 'hi, node';
   yield this.render('index.ejs', {
-    assets: this.assets
+    title: 'Hi, node',
+    entry: 'index',
+    assets: this.assets,
+    props: {}
   })
 };
